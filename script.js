f4e91b7c9fe6cc621480ef54a0f489e23a7bbf63
Kellylaporsh <104526215+Kellylaporsh@users.noreply.github.com>
2022-12-29 02:49:56 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-function accountlinks(){
+
+function navlinks(){
     document.getElementById("navbar").style.right="0%"
 }
 
 function closenavlinks(){
     document.getElementById("navbar").style.right="-200%"
 }
+
+/* Abeg explain these lines of code lolz */
